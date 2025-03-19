6eaff2ed16f2ba70b1383eb0c10dcc36de08ed6f
Daouda-S <daoudasavadogo0908@gmail.com>
2025-03-19 11:44:00 +0000

    ajout du design de la facture

--- app/Http/Controllers/PayementController.php
+++ app/Http/Controllers/PayementController.php
@@ -140,73 +140,87 @@ class PayementController extends Controller
         // Créer une nouvelle instance de TCPDF avec FPDI
         // Ajouter une page
         $pdf->AddPage();
+        $pdf->SetTextColor(252, 146, 80);
+        $pdf->SetFont('Helvetica', 'B', 15);
+        $pdf->Cell(100, 15, 'Facture N°'.$reservation->id, 0, 0, 'L');  // Label "ID Réservation"
+        $pdf->Cell(90, 15, 'Doonya Spaces', 0, 1, 'R');    // Valeur de l'ID de la réservation
 
-        // ID Réservation
-        $pdf->Cell(35, 10, 'ID Réservation :', 0, 0, 'L');  // Label "ID Réservation"
-        $pdf->Cell(30, 10, $reservation->id, 0, 1, 'L');    // Valeur de l'ID de la réservation
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->SetFont('Helvetica', 'I', 10);
+        $pdf->Cell(50, 7, 'Nom : '.$reservation->user->name, 0, 1, 'L');  // Valeur du nom d'utilisateur
+        $pdf->Cell(50, 7, 'Email : '.$reservation->user->email, 0, 1, 'L');  // Valeur du nom d'utilisateur 
+        $pdf->Cell(50, 7, 'Téléphone : '.$reservation->phone, 0, 1, 'L');  // Valeur du nom d'utilisateur
+        $pdf->Cell(50, 7, "Date : " . date('d/m/Y'), 0, 1, 'L');  // Date du jour
+        $pdf->Cell(50, 7, 'Lieu : Bd Tensoba Zoobdo, rue 17.69 porte 333, Secteur 06', 0, 1, 'L');  // Valeur du nom d'utilisateur
+        $pdf->Ln(5);
 
-        // ID Utilisateur   
-        $pdf->Cell(30, 10, 'ID Utilisateur :', 0, 0, 'L');  // Label "ID Utilisateur"
-        $pdf->Cell(30, 10, $reservation->user->name, 0, 1, 'L');  // Valeur du nom d'utilisateur
+        $date = new DateTime($reservation->dateDebut);
+        $dateFormattedDebut = $date->format('d/m/Y');
+        $date = new DateTime($reservation->dateFin);
+        $dateFormattedFin = $date->format('d/m/Y');
 
-        $pdf->Ln(10);
-
-        // Informations sur la réservation - Tableau
-        $pdf->SetFont('Helvetica', '', 12);
+        $pdf->Cell(30, 5,'Réservation du : '.$dateFormattedDebut.' au : '.$dateFormattedFin, 0, 1, 'L');  // Valeur du nom d'utilisateur
+        $pdf->Cell(50, 5, 'Status de la réservation : '.$reservation->status,0, 1, 'L');
+        $pdf->Ln(20);
 
         // Définir l'en-tête du tableau
-        $pdf->SetFillColor(200, 220, 255);  // Couleur de fond pour l'en-tête
-        $pdf->Cell(60, 10, 'ID Réservation', 1, 0, 'C', true);
-        $pdf->Cell(60, 10, 'ID Utilisateur', 1, 0, 'C', true);
-        $pdf->Cell(60, 10, 'ID Espace', 1, 1, 'C', true);  // Changement de ligne
-
-        // Remplir les informations du tableau
         $pdf->SetFont('Helvetica', '', 12);
-        $pdf->Cell(60, 10, $reservation->id, 1, 0, 'C');
-        $pdf->Cell(60, 10, $reservation->user_id, 1, 0, 'C');
-        $pdf->Cell(60, 10, $reservation->espace_id, 1, 1, 'C');
+        $pdf->Cell(80, 10,'Designation', 1, 0, 'C');
+        $pdf->Cell(35, 10,'Nbre de jours', 1, 0, 'C');
+        $pdf->Cell(35, 10,'Prix par jour', 1, 0, 'C');
+        $pdf->Cell(35, 10,'Prix total', 1, 1, 'C');
 
-        // Date de début et date de fin
-        $pdf->Cell(60, 10, 'Date Début', 1, 0, 'C', true);
-        $pdf->Cell(60, 10, 'Date Fin', 1, 0, 'C', true);
-        $pdf->Cell(60, 10, 'Prix', 1, 1, 'C', true);  // Changement de ligne
+        $pdf->SetFont('Helvetica', 'I', 10);
+        $pdf->Cell(80, 10,'Réservation de :','LR',0, 'L');
+        $pdf->Cell(35, 10,'','LR',0, 'L');
+        $pdf->Cell(35, 10,'','LR',0, 'L');
+        $pdf->Cell(35, 10,'','LR',1, 'L');
 
-        $pdf->Cell(60, 10, $reservation->dateDebut, 1, 0, 'C');
-        $pdf->Cell(60, 10, $reservation->dateFin, 1, 0, 'C');
-        $pdf->Cell(60, 10, number_format($reservation->prix, 2) . ' €', 1, 1, 'C');
+        $pdf->Cell(80, 5, $reservation->espace->nom,'LR', 0, 'L');
+        $pdf->Cell(35, 5, '2', 'LR', 0, 'C');
+        $pdf->Cell(35, 5, number_format($reservation->prix, 2) . ' FCFA', 'LR', 0, 'C');
+        $pdf->Cell(35, 5, number_format($reservation->prix, 2) . ' FCFA', 'LR', 1, 'C');
 
-        // Statut et téléphone
-        $pdf->Cell(60, 10, 'Statut', 1, 0, 'C', true);
-        $pdf->Cell(60, 10, 'Téléphone', 1, 1, 'C', true);  // Changement de ligne
-
-        $pdf->Cell(60, 10, $reservation->status, 1, 0, 'C');
-        $pdf->Cell(60, 10, $reservation->phone, 1, 1, 'C');
-
-        // Affichage de l'image de la réservation (facultatif)
-        if ($reservation->image) {
-            $pdf->Cell(0, 10, 'Image de la réservation:', 0, 1);
-            // Ajout de l'image à la facture (si elle existe)
-            $imagePath = storage_path('app/public/' . $reservation->image);
-            $pdf->Image($imagePath, 10, $pdf->GetY(), 40);
-            $pdf->Ln(45);  // Déplace le curseur en dessous de l'image
-        }
-
-        // Options supplémentaires (si disponibles)
         if ($reservation->options->isNotEmpty()) {
-            $pdf->Cell(0, 10, 'Options supplémentaires:', 0, 1);
+            $pdf->Cell(80, 10, 'Options supplémentaires','LR', 0, 'L');
+            $pdf->Cell(35, 10,'','LR',0, 'L');
+            $pdf->Cell(35, 10,'','LR',0, 'L');
+            $pdf->Cell(35, 10,'','LR',1, 'L');
             foreach ($reservation->options as $option) {
-                $pdf->Cell(0, 10, '- ' . $option->option->materiel, 0, 1);
+                $pdf->Cell(80, 5, '- ' . $option->option->materiel, 'LR', 0,'L');
+                $pdf->Cell(35, 5,'','LR',0, 'L');
+                $pdf->Cell(35, 5,'','LR',0, 'L');
+                $pdf->Cell(35, 5,'','LR',1, 'L');
             }
-            $pdf->Ln(5);
         }
+        $pdf->Cell(80, 5,'','LR',0, 'L');
+        $pdf->Cell(35, 5,'','LR',0, 'L');
+        $pdf->Cell(35, 5,'','LR',0, 'L');
+        $pdf->Cell(35, 5,'','LR',1, 'L');
 
-        // Lignes de séparation
-        $pdf->Line(10, $pdf->GetY(), 200, $pdf->GetY());
+        $pdf->Cell(150, 10, 'Total', 1, 0, 'C');
+        $pdf->Cell(35, 10, number_format($reservation->prix, 2) . ' FCFA', 1, 1, 'C');
         $pdf->Ln(10);
 
+        // $pdf->SetFont('Helvetica', 'I', 12);
+        // // Affichage de l'image de la réservation (facultatif)
+        // if ($reservation->image) {
+        //     $pdf->Cell(0, 10, 'Image de la Capture d\'ecran:', 0, 1);
+        //     // Ajout de l'image à la facture (si elle existe)
+        //     $imagePath = storage_path('app/public/' . $reservation->image);
+        //     $pdf->Image($imagePath, 10, $pdf->GetY(), 40);
+        //     $pdf->Ln(45);  // Déplace le curseur en dessous de l'image
+        // }
+        
+        $pdf->Ln(100);
+
+        // Lignes de séparation
+        $pdf->Line(5, $pdf->GetY(), 200, $pdf->GetY());
+        $pdf->Ln(5);
+
         // Remarque
-        $pdf->SetFont('Helvetica', 'I', 10);
-        $pdf->MultiCell(0, 10, 'Remarque: Merci de votre réservation. Nous vous contacterons bientôt pour finaliser la procédure.', 0, 'L');
+        $pdf->SetFont('Helvetica', 'I', 5);
+        $pdf->MultiCell(0, 5, 'Remarque: Merci de votre réservation. Nous vous contacterons bientôt pour finaliser la procédure.', 0, 'L');
 
         // Télécharger le PDF
         $pdf->Output('facture_' . $reservation->id . '.pdf', 'D');
